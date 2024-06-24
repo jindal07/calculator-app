7a0e13f5f6a93f1abbf80cb04a3249f74efed5e5
jindal07 <121694647+jindal07@users.noreply.github.com>
2024-06-24 15:25:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+var btnEl=document.querySelectorAll("button");
+var inputEl=document.querySelector("input");
+
+for( let i=0;i<btnEl.length;i++){
+    btnEl[i].addEventListener("click",function(){
+        var btnValue=btnEl[i].textContent;
+        if(btnValue==="C"){
+            clearResult();
+        }
+        else if(btnValue==="="){
+            calculateResult();
+        }
+        else if(btnValue==="D"){
+            deleteLast();
+        }
+        else{
+            appendValue(btnValue);
+        }
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    var keyValue=event.key;
+    if(key=isFinite(event.key)|| keyValue==="+"||keyValue==="/"||keyValue==="*"||keyValue==="-"){
+        appendValue(keyValue);
+    }
+    else{
+        if(keyValue==="C" || keyValue==="c"){
+            clearResult();
+        }
+        else if(keyValue==="Enter" || keyValue==="="){
+            if(inputEl.value){
+                calculateResult();
+            }
+            else{
+                alert("Please enter a valid expression");
+            }
+        }
+        else if(keyValue==="Backspace"){
+            deleteLast();
+        }
+    }
+    
+})
+
+function deleteLast(){
+    inputEl.value=inputEl.value.substring(0,inputEl.value.length-1);
+}
+
+function appendValue(btnValue){
+    inputEl.value +=btnValue;
+}
+
+function calculateResult(){
+    inputEl.value=eval(inputEl.value);
+}
+function clearResult(){
+    inputEl.value="";
+}
